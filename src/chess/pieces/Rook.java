38d2a149a0c0a64a6b944c69f614389e82debb19
Fagner Souza <fagner.roberto89@gmail.com>
2020-08-18 06:46:04 -0300

    Handling captured pieces

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -33,7 +33,7 @@ public class Rook  extends ChessPiece {
 		}
 		
 		// left		
-		p.setValues(position.getRow()  -1, position.getColumn() - 1);
+		p.setValues(position.getRow() , position.getColumn() - 1);
 		while (getBoard() .positionExists (p) && !getBoard() .thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() -1);
@@ -43,7 +43,7 @@ public class Rook  extends ChessPiece {
 		}
 		
 		// right		
-				p.setValues(position.getRow()  -1, position.getColumn() + 1);
+				p.setValues(position.getRow(), position.getColumn() + 1);
 				while (getBoard() .positionExists (p) && !getBoard() .thereIsAPiece(p)) {
 					mat[p.getRow()][p.getColumn()] = true;
 					p.setColumn(p.getColumn() +1);
